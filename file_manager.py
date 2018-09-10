d0b7a0d71edffa1a9771a972cda191d8b2a87208
RazerKutz <anthonykutz57@gmail.com>
2018-09-10 12:05:20 -0700

    added file manager structure.

---
+++ file_manager.py
@@ -0,0 +1,18 @@
+def load():
+    pass
+
+
+def save():
+    pass
+
+
+def get_fill_path():
+    pass
+
+
+def add_entry():
+    pass
+
+
+def remove_entry():
+    pass
--- program.py
+++ program.py
@@ -1,33 +1,52 @@
 import collections
-import operator
+import file_manager
 
 
 def main():
     print_header()
-    partners = input_partners_from_file()
-    # print_partners(partners)
-    hours_list = input_hours(partners)
-    # print_partners_plus_hours(partners, hours_list)
-    partner_dict = dict(zip(partners, hours_list))
-    # print(partner_dict)
-    # print(sum(dictionary.values()))
-    total_money = float(input('Total tips: '))
-    # calc_tph(sum(dictionary.values()), )
-    dollar_per_hour = calc_tph(sum(partner_dict.values()), total_money)
-    dollar_per_hour = float("%.2f" % dollar_per_hour)
-    print(dollar_per_hour)
-    partner_dict_idph = calc_idph(dollar_per_hour, partner_dict)
-    # print(partner_dict_idph)
-    # print(calc_under(partner_dict_idph))
-    tip_total_under = calc_under(partner_dict_idph)
-    od = sort_dec(partner_dict_idph)
-    od = collections.OrderedDict(od)
-    print(od, type(od))
-    final_dict = distribute_under(od, total_money, tip_total_under)
-    for key, val in final_dict.items():
-        print(key, ": ", val)
+    file_manager.load()
+    run_loop()
 
 
+def run_loop():
+    # todo Check for partners.txt.
+    while True:
+        cmd = input('[E]nter info or [Q]uit: ').lower()
+        if cmd == 'e':
+            partners = input_partners_from_file()
+            # print_partners(partners)
+            hours_list = input_hours(partners)
+            # if the user enters Q then the program closes nicely
+            if hours_list is None:
+                print('quitting program.')
+                break
+            # print_partners_plus_hours(partners, hours_list)
+            partner_dict = dict(zip(partners, hours_list))
+            # print(partner_dict)
+            # print(sum(dictionary.values()))
+            total_money = float(input('Total tips: '))
+            # calc_tph(sum(dictionary.values()), )
+            dollar_per_hour = calc_tph(sum(partner_dict.values()), total_money)
+            dollar_per_hour = float("%.2f" % dollar_per_hour)
+            print(dollar_per_hour)
+            partner_dict_idph = calc_idph(dollar_per_hour, partner_dict)
+            # print(partner_dict_idph)
+            # print(calc_under(partner_dict_idph))
+            tip_total_under = calc_under(partner_dict_idph)
+            od = sort_dec(partner_dict_idph)
+            od = collections.OrderedDict(od)
+            print(od, type(od))
+            final_dict = distribute_under(od, total_money, tip_total_under)
+            for key, val in final_dict.items():
+                print(key, ": ", val)
+        elif cmd == "q":
+            print('quitting program.')
+            break
+        print()
+
+
+
+# This is if there is no file for the partners
 def input_partners_manual():
     count = input('Number of partners: ')
     list = []
@@ -38,8 +57,11 @@ def input_partners_manual():
     return list
 
 
+# reads the list of partners from the partners.txt
 def input_partners_from_file():
-    lines = [line.rstrip('\n') for line in open('partners.txt')]
+    with open('partners.txt') as f:
+        lines = f.readlines()
+    lines = [x.rstrip('\n') for x in lines]
     return lines
 
 
@@ -55,8 +77,13 @@ def print_partners_plus_hours(partners, hours):
 def input_hours(p_list):
     print('Please input the hours worked for each partner.')
     hours = []
+
     for x in range(0, len(p_list)):
-        hours.append(float(input(p_list[x] + ': ')))
+        cmd = input(p_list[x] + ': ')
+        if cmd.lower() == 'q':
+            return None
+        else:
+            hours.append(float(cmd))
 
     return hours
 
