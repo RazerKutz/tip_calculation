4304399e46ab391f7bebd0dfce54f01bf7e1b025
RazerKutz <anthonykutz57@gmail.com>
2018-09-10 19:05:07 -0700

    added comments changed the naming of some peramiters

--- file_manager.py
+++ file_manager.py
@@ -1,10 +1,14 @@
+"""
+
+"""
+
 import os
 
 
 def load(name):
     """
     This method creates and loads partner data.
-    :param name: This base name of the journal to load.
+    :param name: This base name of the partnerfile to load.
     :return: A new journal data structure populated with the file data.
     """
     data = []
@@ -19,6 +23,12 @@ def load(name):
 
 
 def save(name, data):
+    """
+    Saves the data to the file "name"
+    :param name: Name of the file to save to.
+    :param data: the data you want to save to the file.
+    :return: -
+    """
     filename = get_full_pathname(name)
     print("..... saving to: {}".format(filename))
 
@@ -28,15 +38,32 @@ def save(name, data):
 
 
 def get_full_pathname(name):
+    """
+    gets the full path of the file
+    :param name: the file name
+    :return: file path
+    """
     filename = os.path.abspath(os.path.join('.', name + '.prtnrs'))
     return filename
 
 
-def add_entry(text, data):
-    data.append(text)
+def add_entry(name, data):
+    """
+    adds a name to the list
+    :param name: the name to be added.
+    :param data: the list to add the name to.
+    :return: -
+    """
+    data.append(name)
 
 
 def remove_entry(num, data):
+    """
+    removes a specified index from data
+    :param num: the index to remove
+    :param data: the list to remove the index from
+    :return:
+    """
     new_data = []
     del data[num]
     return data
